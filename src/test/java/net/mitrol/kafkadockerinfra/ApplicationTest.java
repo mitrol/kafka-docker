f2eb94f3e1b263a40b61a05ae8dc43c57a6a15d5
Hernán Massad <hernan.massad@mitrol.net>
2017-12-20 15:45:55 -0300

    test cleanup

--- src/test/java/net/mitrol/kafkadockerinfra/ApplicationTest.java
+++ src/test/java/net/mitrol/kafkadockerinfra/ApplicationTest.java
@@ -1,34 +1,23 @@
 package net.mitrol.kafkadockerinfra;
 
-import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
-import org.springframework.kafka.test.rule.KafkaEmbedded;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTest {
 
-    protected final static String TOPIC_NAME = "kafka_docker_test";
-
-//    @Autowired
-//    private KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry;
-//
-//    @Autowired
-//    private Receiver receiver;
+    private final static String TOPIC_NAME = "kafka_docker_test";
+    private final static String MESSAGE = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ¡²³¤€¼½‘’~!@#%$^&*()_+äå®";
 
     @Autowired
     private Sender sender;
 
-    @ClassRule
-    public static KafkaEmbedded kafkaEmbedded = new KafkaEmbedded(1, true, TOPIC_NAME);
-
     @Test
-    public void testReceive() throws Exception {
-        sender.send(TOPIC_NAME, "Hello Spring Kafka!");
+    public void testSender() throws Exception {
+        sender.send(TOPIC_NAME, MESSAGE);
     }
 }
